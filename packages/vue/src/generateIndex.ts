62195de00ef756c9c522f51f42ee4960ed38bd06
omridevk <omrikatz12@gmail.com>
2022-09-14 19:03:50 +0300

    fix: add new index ts pattern

--- packages/vue/src/generateIndex.ts
+++ packages/vue/src/generateIndex.ts
@@ -2,6 +2,8 @@ import path from "path";
 import fse from "fs-extra";
 import globAsync from "fast-glob";
 import { Options } from "@figus/types";
+import { formatFile } from "@figus/utils";
+import { paramCase } from "change-case";
 
 export async function generateIndex({ output }: { output: Options["output"] }) {
     if (!output) {
@@ -14,6 +16,18 @@ export async function generateIndex({ output }: { output: Options["output"] }) {
             return `export { default as ${typename} } from './${typename}.vue';\n`;
         })
         .join("");
-
-    await fse.writeFile(path.join(output, "index.ts"), index);
+    let names: string | string[] = files.map((file) => {
+        const fileName = path.basename(file).replace(".vue", "");
+        const typename = paramCase(fileName);
+        return `
+        '${typename}': {
+          fileName: '${path.basename(fileName)}',
+        }
+      `;
+    });
+    names = ["export const icons = {", names, "}"].join("");
+    const indexWithNames = index.concat(names);
+    const fileName = path.join(output, "index.ts");
+    await fse.writeFile(path.join(output, "index.ts"), indexWithNames);
+    await formatFile(fileName);
 }
